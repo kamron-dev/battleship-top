885fb5436a2cdbb8edb0f6bf74acddfa652e3fd7
kamron-dev <k.kamron@yadnex.ru>
2024-07-05 18:27:00 +0500

    started writing the renderGrid function in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-const myDiv = document.querySelector(".player1");
+import { Player, ComputerPlayer, Ship, GameBoard } from "../battleship.js";
+
+let myDiv = document.querySelector(".player1");
+const me = Player();
+
+const renderGrid = (player) => {
+    const grid = document.createElement("div");
+   
+    player.myBoard.ObjBoard.forEach(object => {
+        const allCells = Object.values(object);
+        for (let cell of allCells) {
+            const button = document.createElement("button");
+            button.classList.add("grid-button");
+            button.innerHTML = cell;
+            grid.appendChild(button);
+        }
+    });
 
 
+    return grid
+};
 
+// console.table(renderGrid(me))
+
+
+myDiv.appendChild(renderGrid(me));
