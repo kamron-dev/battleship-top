74c166d782a3da6a75d5ab6ab860d11b43abefe2
kamron-dev <k.kamron@yadnex.ru>
2024-07-10 11:12:33 +0500

    createTable function working

--- src/index.js
+++ src/index.js
@@ -1,51 +1,43 @@
 import { Player, ComputerPlayer, Ship, GameBoard } from "../battleship.js";
 
-const myDiv = document.querySelector(".player1");
+const myTable = document.querySelector(".battlefield-table-1");
 const opponentsDiv = document.querySelector(".player2");
 const me = Player();
 const opponent = ComputerPlayer();
 
-// me.board.setShipOnBoard(1, "A", 1);
-// me.board.setShipOnBoard(1, "A", 3, "horizontal");
+me.board.setShipOnBoard(1, "A");
+me.board.setShipOnBoard(3, "A");
+me.board.setShipOnBoard(4, "G");
+me.board.setShipOnBoard(5, "B");
 
-const renderGrid = (player) => {
-    const grid = document.createElement("table");
-    grid.classList.add("board-grid-table");
-   
-    player.board.ObjBoard.forEach(object => {
-        const allCells = Object.values(object);
-        for (let cell of allCells) {
-            const button = document.createElement("button");
-            button.classList.add("grid-button");
-            button.innerHTML = cell;
-            grid.appendChild(button);
-        }
-    });
-
-
-    return grid
-};
-
-const renderTable = (player) => {
-    const table = document.createElement("table");
-    const tBody = document.createElement("tbody");
-    table.classList.add("board-grid-table");
-
-    const rows = player.board.ObjBoard;
-    rows.forEach(row => {
-        const tRow = document.createElement("tr");
-        tRow.classList.add("battlefield-row");
-        tBody.appendChild(tRow);
-    })
-    
-
-    table.appendChild(tBody);
-
-    return table;
+const populateTable = (table, player) => {
 
 }
 
-// console.table(renderGrid(me))
+const createTable = () => {
+    const letterCoordinates = "ABCDEFGHIJ".split("");
+    const tBody = document.createElement("tbody");  /// there should be only 1
+    
+    // create 10 trs
+    for (let i = 0; i < 10; i++) {
+        const tr = document.createElement("tr");
+        tr.setAttribute("data-row", i);
+        tr.classList.add("battlefield-row");
+        tBody.appendChild(tr);
 
-// renderTable(me);
-myDiv.appendChild(renderTable(me));
+        for (let j = 0; j < 10; j++) {
+            const td = document.createElement("td");
+            td.setAttribute("data-column", letterCoordinates[j]);
+            td.classList.add("battlefield-cell");
+            const div = document.createElement("div");
+            div.classList.add("battlefiled-cell-content");
+            td.appendChild(div);
+            tr.appendChild(td);
+        };
+    };
+
+    return tBody;
+};
+
+myTable.innerHTML = ""
+myTable.appendChild(createTable());
