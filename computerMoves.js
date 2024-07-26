8d0eac7c727ecb21a113775e413f4087b03235e2
kamron-dev <k.kamron@yadnex.ru>
2024-07-26 13:36:34 +0500

    commit before adding functionality to save shots that hit the target changed some code

--- computerMoves.js
+++ computerMoves.js
@@ -1,8 +1,33 @@
 export const computerMoves = (computerPlayer) => {
     const columns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     const rows = "ABCDEFGHIJ".split("");
-    
     let newMove;
+
+    // helpers that turn row into index and back
+    const rowsIntoIndex = (row) => rows.indexOf(row);
+    const indexIntoRow = (index) => rows[index];
+    
+    // helper that checks if the move is valid
+
+    const checkIfValid = (col, row) => col >= 0 && col < columns.length && row >= 0 && row < rows.length; 
+
+    const createAdjacentMoves = (lastMove) => {
+        const [lastColumn, lastRow] = lastMove;
+        const rowIndex = rowsIntoIndex(lastRow);
+
+        const potentialMoves = [
+            [lastColumn, indexIntoRow(rowIndex + 1)], // Up one row
+            [lastColumn, indexIntoRow(rowIndex - 1)], // Down one row
+            [lastColumn + 1, lastRow], // Left
+            [lastColumn - 1, lastRow] // Right
+        ];
+
+        return potentialMoves.filter(([column, row]) => {
+            return checkIfValid(column, rowIndex(row));
+        });
+    }
+
+    
 
     do {
         newMove = [columns[Math.floor(Math.random() * columns.length)], rows[Math.floor(Math.random() * rows.length)]];
@@ -12,4 +37,5 @@ export const computerMoves = (computerPlayer) => {
    
     computerPlayer.movesMade.push(newMove);
     return newMove;
+
 };
